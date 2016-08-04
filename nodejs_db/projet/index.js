2bef0d1fee55ce446776760809e3df3329149af7
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-08-04 18:41:12 +0200

    recupération des utilisateurs -> node db

--- nodejs_db/projet/index.js
+++ nodejs_db/projet/index.js
@@ -14,16 +14,28 @@ io.on('connection', function(socket){
 
   // AUTHENTIFICATION
   socket.on('auth', function(id, mdp){
-    connection.query('select * from USERS where id = "fmartin"', function(err, rows, fields) {
+    connection.query('select * from USERS where id = "' + id + '"', function(err, rows, fields) {
       if (!err && rows[0].password === mdp) {
         // console.log("Authentification de " + id + " reussie");
         socket.userId = id;
+        socket.parrain = rows[0].parrain;
         socket.emit("auth", true);
       }
       else {
         // console.log("Authentification de " + id + " echouée");
         socket.emit("auth", false);
       }
+    });
+  });
+
+  socket.on('getUsers', function(){
+
+    // ADD CHECK PARRAIN
+    connection.query('select nom, prenom, pseudo, naissance, phrase from USERS where id != "' + socket.userId + '";', function(err, rows, fields) {
+      if (!err)
+        socket.emit("otherUsers", rows);
+      else
+        socket.emit("error", "Impossible d'obtenir les autres utilisateurs");
     });
   });
 
